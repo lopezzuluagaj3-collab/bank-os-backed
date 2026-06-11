cb3d47785f2446e7643de539a3352e315afe8853
Charlie <carlosdueiner1996@gmail.com>
2026-06-11 15:59:53 -0500

    Add AdminUsers table migration

--- Data/Migrations/Master/20260610213048_AddAdminUsers.cs
+++ Data/Migrations/Master/20260610213048_AddAdminUsers.cs
@@ -1,22 +1,39 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace BankOs.Data.Migrations.Master
 {
-    /// <inheritdoc />
     public partial class AddAdminUsers : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "AdminUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminUsers", x => x.Id);
+                });
 
+            migrationBuilder.CreateIndex(
+                name: "IX_AdminUsers_Email",
+                table: "AdminUsers",
+                column: "Email",
+                unique: true);
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(name: "AdminUsers");
         }
     }
 }
